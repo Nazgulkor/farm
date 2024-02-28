5905001c01b0b45efd801e677279a979c38a68ac
nazgulkor <usupmezhiev95@gmail.com>
2024-02-28 10:01:02 +0300

    final

--- main.php
+++ main.php
@@ -8,60 +8,34 @@ use App\Farm\Livestocks\CowLivestock;
 
 $farm = new Farm;
 
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
+for($i = 0; $i <= 20; $i++){
 
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-$farm->getBarn()->addLivestock(new CowLivestock);
-
+    $farm->getBarn()->addLivestock(new ChickenLivestock);
+}
+for($i = 0; $i <= 10; $i++){
+    $farm->getBarn()->addLivestock(new CowLivestock);
+}
 echo "Вывести на экран информацию о количестве каждого типа животных на ферме.\n";
 $farm->countAllLivestockUniques();
 
 // 7 раз (неделю) произвести сбор продукции (подоить коров и собрать яйца у кур).
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-
+for($i = 0; $i <= 7; $i++){
+    $farm->getBarn()->collectProducts();
+}
 echo "Вывести на экран общее кол-во собранной за неделю продукции каждого типа.\n";
 $farm->countAllProducUniques();
 
 // Добавить на ферму ещё 5 кур и 1 корову (съездили на рынок, купили животных).
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
-$farm->getBarn()->addLivestock(new ChickenLivestock);
+for($i = 0; $i <= 7; $i++){
+    $farm->getBarn()->addLivestock(new ChickenLivestock);
+}
 $farm->getBarn()->addLivestock(new CowLivestock);
-
 echo "Снова вывести информацию о количестве каждого типа животных на ферме.\n";
 $farm->countAllLivestockUniques();
 
-echo "Снова 7 раз (неделю) производим сбор продукции и выводим результат на экран.\n";
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
-$farm->getBarn()->collectProducts();
 
+echo "Снова 7 раз (неделю) производим сбор продукции и выводим результат на экран.\n";
+for($i = 0; $i <= 7; $i++){
+    $farm->getBarn()->collectProducts();
+}
 $farm->countAllProducUniques();
